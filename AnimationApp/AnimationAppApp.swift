dd7a0fbe299bbe5b29c49bd9ecd0fbf99a8c2772
KremnevViacheslav <rusted2192@gmail.com>
2022-05-12 23:08:48 +0300

    Initial Commit

---
+++ AnimationApp/AnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppApp.swift
+//  AnimationApp
+//
+//  Created by Вячеслав Кремнев on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
